632117e8d39b91ce9efba646c05379872278eca7
francof <facionifranco@gmail.com>
2013-07-18 18:20:27 -0300

    verbatim hw4 version

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname + '/assets'));
 app.get('/', function(request, response) {
 	var buffer = fs.readFileSync('index.html');
 	response.send(buffer.toString());
